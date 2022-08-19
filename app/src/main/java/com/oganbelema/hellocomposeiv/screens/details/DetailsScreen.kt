13272eda157ccaf503362d57ac911727f01b45ed
Oganbelema <oganbelema@gmail.com>
2022-08-19 04:22:43 +0100

    styled the details screen

--- app/src/main/java/com/oganbelema/hellocomposeiv/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/oganbelema/hellocomposeiv/screens/details/DetailsScreen.kt
@@ -1,12 +1,25 @@
 package com.oganbelema.hellocomposeiv.screens.details
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun DetailsScreen(navController: NavController, movie: String?) {
     if (movie != null) {
-        Text(text = movie)
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+
+                Text(text = movie, style = MaterialTheme.typography.h5)
+            }
+        }
     }
 }
