edb4bd757f7b03ad462944e067f102860a59cf45
Oganbelema <oganbelema@gmail.com>
2022-08-19 04:01:41 +0100

    added navigation frrom movie list to detail screen

--- app/src/main/java/com/oganbelema/hellocomposeiv/navigation/MovieNavigation.kt
+++ app/src/main/java/com/oganbelema/hellocomposeiv/navigation/MovieNavigation.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.oganbelema.hellocomposeiv.screens.details.DetailsScreen
+import com.oganbelema.hellocomposeiv.screens.home.HomeScreen
 
 @Composable
 fun MovieNavigation() {
@@ -11,7 +13,11 @@ fun MovieNavigation() {
     NavHost(navController = navController,
         startDestination = MovieScreens.HomeScreen.name) {
         composable(MovieScreens.HomeScreen.name) {
-            
+            HomeScreen(navController = navController)
+        }
+
+        composable(MovieScreens.DetailsScreen.name) {
+            DetailsScreen(navController = navController)
         }
     }
 }
